64d4c91c1cd385a6fd7114b497a9f83e5acafe76
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-16 23:12:18 +0300

    products.models added

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,15 @@
 import json
 from django.http import JsonResponse
 
+from products.models import Product
 
 def api_home(request, *args, **kwargs):
-    # request.body
-    print(request.GET) # url query params
-    print(request.POST)
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-    try:
-        data = json.loads(body) # string of JSON data -> Python dict
-    except:
-        pass
-    print(data)
-    data['params'] = dict(request.GET)
-    data['headers'] = dict(request.headers)
-    data['content_type'] = request.content_type
+    if model_data:
+        data['id'] = model_data.id
+        data['title'] = model_data.title
+        data['content'] = model_data.content
+        data['price'] = model_data.price
+        
     return JsonResponse(data)
