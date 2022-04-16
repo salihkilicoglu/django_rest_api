273d7cf3033a588fbdefa83389af84e292b33115
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-17 00:32:48 +0300

    serializers

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,4 @@
+from msilib.schema import Property
 from django.db import models
 
 # Create your models here.
@@ -5,3 +6,10 @@ class Product(models.Model):
     title= models.CharField(max_length=120) 
     content= models.TextField(blank=True, null=True)
     price= models.DecimalField(max_digits=15, decimal_places=2, default=99.99)
+
+    @property
+    def sale_price(self):
+        return "%.2f" %(float(self.price) * 0.8)
+    
+    def get_discount(self):
+        return "131"
