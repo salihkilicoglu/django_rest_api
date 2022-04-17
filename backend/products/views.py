e70494b392487ff1fc22a596e76a3b4787736204
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-18 01:00:27 +0300

    Delete and UpdateAPIView for api/products/{product_id}/delete/ and /update/

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,12 +1,11 @@
-from django.http import Http404
 from rest_framework import generics
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-# from django.http import Http404
 from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
+
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
@@ -22,12 +21,40 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
 
 product_list_create_view = ProductListCreateAPIView.as_view()
 
+
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
 
 product_detail_view = ProductDetailAPIView.as_view()
+
+
+class ProductUpdateAPIView(generics.UpdateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_update(self, serializer):
+        instance = serializer.save()
+        if not instance.content:
+            instance.content = instance.title
+
+
+product_update_view = ProductUpdateAPIView.as_view()
+
+
+class ProductDestroyAPIView(generics.DestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'pk'
+
+    def perform_destroy(self, instance):
+        # instance
+        super().perform_destroy(instance)
+            
+
+product_destroy_view = ProductDestroyAPIView.as_view()
 
 
 # class ProductListAPIView(generics.ListAPIView):
