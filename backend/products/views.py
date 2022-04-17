f2f0836743ba5dbc8b7ba00f61aa07b64078474b
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-17 19:49:41 +0300

    ProductCreateAPIView -> /api/products/[POST]

--- backend/products/views.py
+++ backend/products/views.py
@@ -4,11 +4,23 @@ from rest_framework import generics
 from .models import Product
 from .serializers import ProductSerializer
 
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        #serializer.save(user=self.request.user)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content') or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_create_view = ProductCreateAPIView.as_view()
 
 class ProductDetailAPIView(generics.RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # lookup_field = 'pk'
-
 
 product_detail_view = ProductDetailAPIView.as_view()
