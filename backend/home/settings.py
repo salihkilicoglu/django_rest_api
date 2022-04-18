c27cc79ab265ac645394aff8cfb0a25fd165e73d
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-18 04:39:24 +0300

    authentication

--- backend/home/settings.py
+++ backend/home/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from distutils.log import debug
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,8 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',   
     'api',
-    'products',
+    'products'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
+        "api.authentication.TokenAuthentication"
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly"
+    ]
+}
--- py_client/create.py
+++ py_client/create.py
@@ -1,5 +1,8 @@
 import requests
 
+
+headers = {'Authorization': 'Bearer cf66b4d6b146cc5818bc165e61214e8faed6e8d2'}
+
 endpoint = "http://localhost:8000/api/products/"
 
 data = {
@@ -7,5 +10,5 @@ data = {
     "price": 32.99
 }
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.post(endpoint, json=data, headers=headers)
 print(get_response.json())
