56ef4f89c40c059a5bf628e5f5127cffc1e5fb0a
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-18 03:15:20 +0300

    Permissions

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,15 +1,18 @@
-from rest_framework import generics, mixins
+from rest_framework import authentication, generics, mixins, permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 
 from .models import Product
+from .permissions import IsStaffEditorPermission
 from .serializers import ProductSerializer
 
 
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [authentication.SessionAuthentication]
+    permission_classes = [permissions.IsAdminUser, IsStaffEditorPermission]
 
     def perform_create(self, serializer):
         #serializer.save(user=self.request.user)
@@ -67,6 +70,7 @@ product_destroy_view = ProductDestroyAPIView.as_view()
 # product_list_view = ProductListAPIView.as_view()
 
 
+# Mixin Views
 class ProductMixinView(
     mixins.CreateModelMixin,
     mixins.ListModelMixin,
