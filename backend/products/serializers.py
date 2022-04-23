d9a7e874f40bd67227be06e666b38d98f9e55b75
salihkilicoglu <salihkilicoglu@gmail.com>
2022-04-24 00:05:36 +0300

    search app & api/search/ endpoint

--- backend/products/serializers.py
+++ backend/products/serializers.py
@@ -33,6 +33,7 @@ class ProductSerializer(serializers.ModelSerializer):
             'content',
             'price',
             'sale_price',
+            'public',
         ]
     def get_edit_url(self,obj):
         request = self.context.get('request')
